b050b7c59b2bfe6df1527852669f044add41cfcc
誌小強 <oncwnuP0rnVMPHgiWqo-39AWDsN0@git.weixin.qq.com>
2019-11-08 11:30:20 +0800

    feat: add service

---
+++ app/service/tasks.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const Service = require('egg').Service;
+
+class Tasks extends Service {
+  /**
+   * 获取需推送的消息插入数据库
+   */
+  async getDataAndInsertDB(data = {}) {
+    const { ctx } = this;
+    const { success, data: feedCardList, error } = await ctx.service.home.githubTrendingList(data);
+    if (success) {
+      const res = await ctx.service.news.insertFedNews(
+        feedCardList || [],
+        'github'
+      );
+      ctx.body = res;
+    } else {
+      ctx.body = { success: false, error: error };
+    }
+  }
+
+  /**
+   * github trending list
+   */
+  async getDataAndPush(data = {}) {
+    const { ctx } = this;
+    try {
+      const { data: isHoliday } = await ctx.service.home.isHoliday();
+      if (!isHoliday) {
+        const { data: result } = await ctx.service.news.getFedNews({
+          offset: 0,
+          pageSize: 5,
+          ...data,
+        });
+        await ctx.service.github.createIssues({
+          owner: 'zhixiaoqiang',
+          repo: [ 'fed-news-push', 'fedNewsPushServer' ],
+          title: result.title,
+          body: result.text,
+          labels: [ '日报' ],
+        });
+
+        await ctx.service.news.insertFedNewsDay({
+          type: 'markdown',
+          title: result.title,
+          text: result.text,
+        });
+
+        await ctx.service.dingTalk.markdown({
+          title: result.title,
+          text: `${
+            result.text
+          } \n\n #### [查看更多](https://github.com/zhixiaoqiang/fed-news-push/issues)`,
+        });
+        ctx.body = { success: true, data: '发送成功' };
+      } else {
+        ctx.body = { success: true, data: '今天是休息日，暂无推送' };
+      }
+
+    } catch (error) {
+      console.warn(error);
+      ctx.body = { code: 0, success: false, error: error };
+    }
+  }
+
+  async resetHasPushProp() {
+    const { ctx } = this;
+    const result = await ctx.service.news.resetHasPushProp();
+    ctx.body = result;
+  }
+}
+module.exports = Tasks;
